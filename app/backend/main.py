c267798ec930ff75dad45577f5196cb3a64723c1
AegeanBlue <106996288+AegeanBlue@users.noreply.github.com>
2024-09-29 19:12:58 +0300

    nginx and flask are now friends

--- app/backend/main.py
+++ app/backend/main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/app')
 def hello():
     return 'Hello, World!'
 
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=9999)
+    app.run(host='0.0.0.0', port=9999)
+
